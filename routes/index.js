498724293d843386e63a7c927646bb31cca22d8c
jjwyse <josh@cloud-elements.com>
2014-08-26 16:53:14 -0600

    working on loading ALL objects

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,3 @@
-var https = require('https');
-
 exports.index = function (req, res) {
    res.render('index', { title: 'Joshua Wyse' });
-};
-
-exports.list = function (stravaToken) {
-   return function(request, response) {
-
-      var options = {
-         host: 'www.strava.com',
-         path: '/api/v3/athlete/activities',
-         headers: {
-            'User-Agent:': 'strava-map',
-            'Authorization': 'Bearer ' + stravaToken
-         }
-      };
-
-      https.get(options, function(res) {
-         console.log("Strava response code: " + res.statusCode);
-
-         var responseString = "";
-
-         res.on('data', function(data){
-            responseString += data;
-         });
-
-         res.on('end', function(){
-            var json = JSON.parse(responseString);
-            response.send(json);
-         });
-      }).on('error', function(error){
-         console.error(error);
-         response.send(error)
-      });
-   };
 };
